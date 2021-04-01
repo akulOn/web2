f041908d759f64b8280861af348958fb4542e163
akulOn <dracaluka12@gmail.com>
2021-04-01 10:56:35 +0200

    Osposobio sam komunikaciju sa bazom
    Aplikacija za sad samo kupi podatke za ekipu

---
+++ api/webAPI/WebApplication1/Controllers/EkipaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class EkipaController : ApiController
+    {
+        // GET: Ekipa
+        public HttpResponseMessage Get()
+        {
+            string query = @"
+                    select idEkipe, Naziv from dbo.Ekipa
+                    ";
+            DataTable table = new DataTable();
+
+            using(var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["EkipaAppDB"].ConnectionString))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        command.CommandType = CommandType.Text;
+                        adapter.Fill(table);
+                    }
+                }
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, table);
+
+        }
+    }
+}
---
+++ api/webAPI/WebApplication1/Models/Ekipa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Ekipa
+    {
+        public int IdEkipe { get; set; }
+        public string Naziv { get; set; }
+    }
+}
