438e3563798325325b1e3d2618c5f0a51d34bb5e
akulOn <dracaluka12@gmail.com>
2021-04-01 13:56:51 +0200

    Put za Korisnik

--- api/webAPI/WebApplication1/Controllers/KorisnikController.cs
+++ api/webAPI/WebApplication1/Controllers/KorisnikController.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Controllers
     public class KorisnikController : ApiController
     {
         // GET: Korisnik
-        public HttpResponseMessage Get()
+        public HttpResponseMessage Get() // zapravo je GetAll
         {
             // trebalo bi da se radi sa procedrama na bazi, nije dobro da ovde direktno kucam SQL upite
             string procedure = "dbo.GetAllKorisnik";
@@ -43,6 +43,45 @@ namespace WebApplication1.Controllers
         public HttpResponseMessage Post(Korisnik korisnik) // ako dodas vec postojeceg korisnika nece baciti error, ali nece ga dodati u bazu
         {
             string procedure = "dbo.InsertKorisnik";
+            DataTable table = new DataTable();
+            try
+            {
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ElektroDistribucijaAppDB"].ConnectionString))
+                {
+                    using (var command = new SqlCommand(procedure, connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // 3. add parameter to command, which will be passed to the stored procedure
+                        command.Parameters.Add(new SqlParameter("@idKorisnika", korisnik.IdKorisnika));
+                        command.Parameters.Add(new SqlParameter("@korisnickoIme", korisnik.KorisnickoIme));
+                        command.Parameters.Add(new SqlParameter("@email", korisnik.Email));
+                        command.Parameters.Add(new SqlParameter("@lozinka", korisnik.Lozinka));
+                        command.Parameters.Add(new SqlParameter("@ime", korisnik.Ime));
+                        command.Parameters.Add(new SqlParameter("@prezime", korisnik.Prezime));
+                        command.Parameters.Add(new SqlParameter("@datumRodenja", korisnik.DatumRodenja));
+                        command.Parameters.Add(new SqlParameter("@adresa", korisnik.Adresa));
+                        command.Parameters.Add(new SqlParameter("@idTipKorisnika", korisnik.IdTipKorisnika));
+                        command.Parameters.Add(new SqlParameter("@idSlika", korisnik.IdSlika));
+
+                        using (var adapter = new SqlDataAdapter(command))
+                        {
+                            adapter.Fill(table);
+                        }
+                    }
+                }
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        public HttpResponseMessage Put(Korisnik korisnik) // nece baciti error ako korisnik ne postoji u bazi
+        {
+            string procedure = "dbo.UpdateKorisnik";
             DataTable table = new DataTable();
             try
             {
