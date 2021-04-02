1605cbdf71dcb9f99d44b857ddc6f567dacc0e66
akulOn <dracaluka12@gmail.com>
2021-04-02 13:57:34 +0200

    Oprema
    Dodao sam Get, GetAll, Post operacije za operemu

---
+++ api/webAPI/WebApplication1/Controllers/OpremaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class OpremaController : ApiController
+    {
+        [Route("api/Oprema/GetAll")]
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            // trebalo bi da se radi sa procedrama na bazi, nije dobro da ovde direktno kucam SQL upite
+            string query = @"
+                    select * from Oprema
+                    ";
+            DataTable table = new DataTable();
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ElektroDistribucijaAppDB"].ConnectionString))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        command.CommandType = CommandType.Text;
+                        adapter.Fill(table);
+                    }
+                }
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, table);
+        }
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            // trebalo bi da se radi sa procedrama na bazi, nije dobro da ovde direktno kucam SQL upite
+            string query = @"
+                    select * from dbo.Oprema where idOpreme = " + id
+                    ;
+            DataTable table = new DataTable();
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ElektroDistribucijaAppDB"].ConnectionString))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        command.CommandType = CommandType.Text;
+                        adapter.Fill(table);
+                    }
+                }
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, table);
+        }
+        [HttpPost]
+        public HttpResponseMessage Post(Oprema oprema)
+        {
+            string query = @"
+                    insert into dbo.Oprema(idTipOpreme, Kordinate, Adresa) values (" + oprema.IdTipOpreme + ", + '" + oprema.Kordinate + "', +  '" + oprema.Adresa + "')"
+                    ;
+            DataTable table = new DataTable();
+            try
+            {
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ElektroDistribucijaAppDB"].ConnectionString))
+                {
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        using (var adapter = new SqlDataAdapter(command))
+                        {
+                            command.CommandType = CommandType.Text;
+                            adapter.Fill(table);
+                        }
+                    }
+                }
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ api/webAPI/WebApplication1/Models/Oprema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Oprema
+    {
+        // dodaje se u bazi
+        //public int IdOpreme { get; set; }
+        //public string Naziv { get; set; }
+        public int IdTipOpreme { get; set; }
+        public string Kordinate { get; set; }
+        public string Adresa { get; set; }
+
+    }
+}
